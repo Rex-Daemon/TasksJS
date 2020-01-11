051c22495c33cbd8b8674170cfb59e65aee2ef4c
Rakshit Airani M <rakshit.airani.m@gmail.com>
2020-01-11 08:48:47 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+StoredTaskList = [
+    {
+        taskText: 'Clean shelf '
+    },
+    {
+        taskText: 'Pay bills'
+    },
+]
+
+window.onload = () => {
+    
+    const taskList = document.querySelector('#task-list');
+    displayTaskList(StoredTaskList, taskList);
+}
+
+// Display the Tasks
+function displayTaskList(StoredList, parentDiv){
+    for (let i = 0; i < StoredList.length; i++) {
+        constructTaskList(StoredList[i].taskText, parentDiv);
+    }
+}
+
+// Construct taskList
+function constructTaskList(taskText, parentDiv) {
+
+    // Consrtuct Task div
+    const divTask = document.createElement('div');
+    divTask.classList.add('task');
+    // Construct task text
+    const divTaskText = document.createElement('p');
+    divTaskText.classList.add('taskText');
+    divTaskText.innerText = taskText;
+    // Construct checkbox input
+    const divTaskCheckbox = document.createElement('input');
+    divTaskCheckbox.type = 'checkbox';
+    divTaskCheckbox.classList.add('taskCheckbox');
+    divTaskCheckbox.addEventListener('click', function () {
+        onCheck(divTaskCheckbox, divTaskText);
+    });
+    // Append
+    divTask.appendChild(divTaskCheckbox);
+    divTask.appendChild(divTaskText);
+    parentDiv.appendChild(divTask);
+}
+
+function onCheck(checkbox, text) {
+    if (checkbox.checked == true) {
+        text.style.textDecoration = 'line-through';
+        text.style.color = '#777777'
+    }
+    else {
+        text.style.textDecoration = 'none';
+        text.style.color = '#000000'
+    }
+}
