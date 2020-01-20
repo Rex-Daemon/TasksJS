94791494b883eacd57980e58bc31d5bf039e7cec
Rakshit Airani M <rakshit.airani.m@gmail.com>
2020-01-20 13:23:27 +0530

    Build Commit

---
+++ precache-manifest.9dcf22fb3f3ee9c6d4d0ff6a67471e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f414520b4d8a007383982620f6b7c2e9",
+    "url": "/TasksJS/index.html"
+  },
+  {
+    "revision": "9c11932818d779b26fe9",
+    "url": "/TasksJS/static/css/main.10fd3a4e.chunk.css"
+  },
+  {
+    "revision": "edb61a92a835fccb6095",
+    "url": "/TasksJS/static/js/2.4bc788f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TasksJS/static/js/2.4bc788f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c11932818d779b26fe9",
+    "url": "/TasksJS/static/js/main.6619652e.chunk.js"
+  },
+  {
+    "revision": "ace43f9e6d78d9c5e0e6",
+    "url": "/TasksJS/static/js/runtime-main.355ccfea.js"
+  }
+]);
